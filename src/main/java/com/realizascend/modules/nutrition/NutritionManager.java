f7a95cc9ebfede5385171d317c6cf0f3f1c1f471
wisemanjr4 <wisemanjr4@users.noreply.github.com>
2026-08-30 18:36:00 +0900

    ビルド修正: EventPriority import追加

--- src/main/java/com/realizascend/modules/nutrition/NutritionManager.java
+++ src/main/java/com/realizascend/modules/nutrition/NutritionManager.java
@@ -13,6 +13,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.block.Furnace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
