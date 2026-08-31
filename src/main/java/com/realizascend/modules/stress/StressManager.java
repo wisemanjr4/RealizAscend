c68796009567c2a789af6074d8e5d2d228490e9b
wisemanjr4 <wisemanjr4@users.noreply.github.com>
2026-09-01 00:09:43 +0900

    ビルド修正: EntityDamageEvent import追加

--- src/main/java/com/realizascend/modules/stress/StressManager.java
+++ src/main/java/com/realizascend/modules/stress/StressManager.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
